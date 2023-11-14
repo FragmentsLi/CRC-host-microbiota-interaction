6eac4c5e3f46d24a4063c72f652d14d6fe02f616
FragmentsLi <127385089+FragmentsLi@users.noreply.github.com>
2023-11-14 21:20:37 +0800

    Update and rename Normalization.R to Preprocessing.R

---
+++ Preprocessing.R
@@ -0,0 +1,53 @@
+######   Normalization   ######
+
+#Host gene expression matrix
+data<-read.csv('TCGAbiolinks_RSEM_normalized_data.csv',row.names=1,check.names=F)
+meta_RNA<-read.csv('meta_RNA_with_submitter_id_barcode.csv',row.names=1)
+data<-data[,rownames(meta_RNA)]
+
+#log2
+data<-as.matrix(log2(data+1))
+
+#ComBat
+library(sva)
+combat_data <- ComBat(dat = data, batch = meta_RNA$platform)
+
+write.csv(combat_data,'TCGAbiolinks_combat_data_tumor.csv',row.names=T)
+
+######   CMS classification   ######
+
+data<-read.csv('TCGAbiolinks_combat_data_tumor.csv',row.names=1,check.names=F)
+
+library(clusterProfiler)
+library(org.Hs.eg.db)
+map<- bitr(rownames(data), fromType = "SYMBOL",toType = c("ENTREZID"),OrgDb = org.Hs.eg.db)
+
+data_id<-lapply(unique(map$ENTREZID),function(x){return(data[subset(map,map$ENTREZID==x)[1,]$SYMBOL,])})
+data_id_matrix<-do.call("rbind", data_id)
+rownames(data_id_matrix)<-unique(map$ENTREZID)
+
+# CMSclassifier
+library(CMSclassifier)
+rna<-data_id_matrix
+cms_label<-read.delim('cms_labels_public_all.txt')
+cms_label<-subset(cms_label,cms_label$dataset=='tcga')
+rownames(cms_label)<-cms_label$sample
+
+library(stringr)
+patient_id<-unlist(lapply(colnames(rna),function(x){substr(x,1,12)}))
+cms_class<-unlist(lapply(patient_id,function(x)
+  if(x %in% cms_label$sample)return(cms_label[x,5])
+  else return('')))
+
+#Random forest
+Rfcms <- CMSclassifier::classifyCMS(rna,method="RF")[[3]]
+cms_predict<-unlist(lapply(colnames(rna),function(x){Rfcms[x,'RF.predictedCMS']}))
+cms_nearest<-unlist(lapply(colnames(rna),function(x){Rfcms[x,'RF.nearestCMS']}))
+cms_merge<-unlist(lapply(patient_id,function(x)
+  if(x %in% cms_label$sample)return(cms_label[x,5])
+  else return(Rfcms[x,'RF.predictedCMS'])))
+cms_merge[which(is.na(cms_merge))]='NOLBL'
+  
+info<-data.frame(barcode=barcode,patient_id=patient_id,cms_class=cms_class,
+                 cms_predict=cms_predict,cms_nearest=cms_nearest,cms_merge=cms_merge)
+write.csv(info,'TCGA_microbes/sample_info_no_multibam_tumor_rf.csv',row.names=F)                         
